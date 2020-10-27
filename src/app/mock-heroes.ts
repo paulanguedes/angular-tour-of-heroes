7f85e0b5bd6f175411797de89f931fafdeea120a
paulanguedes <paulanguedes@gmail.com>
2020-10-27 13:39:01 -0300

    display hero cards

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,13 +2,13 @@ import { from } from 'rxjs';
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-    { id: 1, name: 'Alisha' },
-    { id: 2, name: 'Lyja' },
-    { id: 3, name: 'Bella Donna' },
-    { id: 4, name: 'Kelly' },
-    { id: 5, name: 'Chastity' },
-    { id: 6, name: 'Jess' },
-    { id: 7, name: 'Doll Girl' },
-    { id: 8, name: 'Huntress' },
-    { id: 9, name: 'Firestar' }
+    { id: 1, name: 'Alisha', img: '../assets/img/Alisha.png' },
+    { id: 2, name: 'Lyja', img: '../assets/img/Lyja.png' },
+    { id: 3, name: 'Bella Donna', img: '../assets/img/BellaDonna.png' },
+    { id: 4, name: 'Kelly', img: '../assets/img/Kelly.png' },
+    { id: 5, name: 'Chastity', img: '../assets/img/Chastity.png' },
+    { id: 6, name: 'Jess', img: '../assets/img/Jess.png' },
+    { id: 7, name: 'Doll Girl', img: '../assets/img/DollGirl.png' },
+    { id: 8, name: 'Huntress', img: '../assets/img/Huntress.png' },
+    { id: 9, name: 'Firestar', img: '../assets/img/Firestar.png' }
 ];
