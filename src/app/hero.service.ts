9d781721262b7cb7e43e24e824af05b58982d3a3
paulanguedes <paulanguedes@gmail.com>
2020-11-02 17:39:56 -0300

    update hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,7 +5,6 @@ import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -13,22 +12,51 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  private heroesUrl = 'api/heroes';
+  private heroesUrl = 'api/heroes'; // URL to web API
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(
     private http: HttpClient,
     private messageService: MessageService
   ) { }
 
-  private log(message: string) {
-    this.messageService.add('HeroService: ${message}');
-  }
-
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
     .pipe(
       tap(_ => this.log('fetched heroes')),
       catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+  }
+
+  /** GET hero by id. Return `undefined` when id not found */
+  getHeroNo404<Data>(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/?id=${id}`;
+    return this.http.get<Hero[]>(url)
+      .pipe(
+        map(heroes => heroes[0]), // returns a {0|1} element array
+        tap(h => {
+          const outcome = h ? `fetched` : `did not find`;
+          this.log(`${outcome} hero id=${id}`);
+        }),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const url = '${this.heroesUrl}/${id}';
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log('fetched hero id=${id}')),
+      catchError(this.handleError<Hero>('getHero id=${id}'))
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log('updated hero ig=${hero.id}')),
+      catchError(this.handleError<any>('updateHerp'))
     );
   }
 
@@ -40,14 +68,10 @@ export class HeroService {
       this.log('${operation} failed: ${error.message}');
       // let the app keep running by returning an empty result
       return of(result as T);
-    }
+    };
   }
 
-  getHero(id: number): Observable<Hero> {
-    const url = '${this.heroesUrl}/${id}';
-    return this.http.get<Hero>(url).pipe(
-      tap(_ => this.log('fetched hero id=${id}')),
-      catchError(this.handleError<Hero>('getHero id=${id}'))
-    );
+  private log(message: string) {
+    this.messageService.add('HeroService: ${message}');
   }
 }
