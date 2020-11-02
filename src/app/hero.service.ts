69522ab8b65a4e6192c2b75a60c833b048cad496
paulanguedes <paulanguedes@gmail.com>
2020-11-02 18:05:41 -0300

    add hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -60,6 +60,13 @@ export class HeroService {
     );
   }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log('added hero w/ id=${newHero.id}')),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       // TODO: send the error to remote logging infrastructure
